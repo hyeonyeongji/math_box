f77b8edde2a6bae3164aa8c19704e7b91d36c625
hyeonyeongji <gusdud227@naver.com>
2025-05-20 22:50:44 +0900

    Make readme.md look nice

--- calculator.py
+++ calculator.py
@@ -5,7 +5,12 @@ def subtract(a,b):
 	return a-b
 def multiply(a,b):
 	return a*b
+<<<<<<< HEAD
 def divide_free(a,b):
 	return a/b
 def squrqre(a):
 	return a*a
+=======
+def divide(a,b):
+	return a/b
+>>>>>>> parent of 2e83a9a (Change divide funtionas free)
